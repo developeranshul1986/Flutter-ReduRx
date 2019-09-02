ecd5eb30b5d7ee9fd59ee35a315a1165f5cf97c1
Wes <wes@cosmicpangolin.com>
2019-09-02 14:04:22 -0700

    /// - Removed nullable property and the {return Container();} statement in /// StreamBuilder, so that the library naturally supports null state objects and /// rendering in slivers (containers throw errors) /// - Added initialData in StreamBuilder so that initial builds don't throw null /// before the stream completes. See /// https://docs.flutter.io/flutter/widgets/StreamBuilder-class.html

--- lib/flutter_redurx.dart
+++ lib/flutter_redurx.dart
@@ -12,43 +12,44 @@
 library flutter_redurx;
 
 import 'dart:async';
+import 'package:meta/meta.dart';
 
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' as flutter;
 import 'package:redurx/redurx.dart';
 
 export 'package:redurx/redurx.dart';
 
 /// Provider Widget to be on top of the App ([child]) providing the State from a given [store].
-class Provider<T> extends InheritedWidget {
+class Provider<T> extends flutter.InheritedWidget {
   Provider({
-    Key key,
-    @required Widget child,
+    flutter.Key key,
+    @required flutter.Widget child,
     @required this.store,
   }) : super(key: key, child: child);
   final Store<T> store;
 
   /// Gets the Provider from a given [BuildContext].
-  static Provider<T> of<T>(BuildContext context) =>
+  static Provider<T> of<T>(flutter.BuildContext context) =>
       context.inheritFromWidgetOfExactType(_targetType<Provider<T>>());
 
   static _targetType<T>() => T;
 
   /// Sugar to dispatch Actions on the Store in the Provider of the given [context].
-  static Store<T> dispatch<T>(BuildContext context, ActionType action) =>
+  static Store<T> dispatch<T>(flutter.BuildContext context, ActionType action) =>
       Provider.of<T>(context).store.dispatch(action);
 
   /// We never trigger update, this is all up to ReduRx.
   @override
-  bool updateShouldNotify(InheritedWidget oldWidget) => false;
+  bool updateShouldNotify(flutter.InheritedWidget oldWidget) => false;
 }
 
 /// The Widget that connects the State to a [builder] function.
-class Connect<S, P> extends StatefulWidget {
+class Connect<S, P> extends flutter.StatefulWidget {
   /// [convert] is how you map the State to [builder] props.
   /// With [where] you can filter when the Widget should re-render, this is very important!
   /// If you want to handle [null] values on the [builder] by yourself, set [nullable] to [true].
   Connect({
-    Key key,
+    flutter.Key key,
     @required this.convert,
     @required this.where,
     @required this.builder,
@@ -56,17 +57,17 @@ class Connect<S, P> extends StatefulWidget {
 
   final P Function(S state) convert;
   final bool Function(P oldState, P newState) where;
-  final Widget Function(P state) builder;
+  final flutter.Widget Function(P state) builder;
 
   @override
   _ConnectState createState() => _ConnectState<S, P>(where, builder);
 }
 
-class _ConnectState<S, P> extends State<Connect<S, P>> {
+class _ConnectState<S, P> extends flutter.State<Connect<S, P>> {
   _ConnectState(this.where, this.builder);
 
   final bool Function(P oldState, P newState) where;
-  final Widget Function(P state) builder;
+  final flutter.Widget Function(P state) builder;
 
   P _prev;
   Store<S> _store;
@@ -83,8 +84,8 @@ class _ConnectState<S, P> extends State<Connect<S, P>> {
   }
 
   @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<P>(
+  flutter.Widget build(flutter.BuildContext context) {
+    return flutter.StreamBuilder<P>(
       initialData: widget.convert(Provider.of<S>(context).store.state),
       stream: _stream,
       builder: (context, snapshot) {
